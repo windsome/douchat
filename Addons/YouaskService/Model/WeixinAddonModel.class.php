5e92b27dfe3c22b0d1641249163a12a71d769e22
mengxing <1248202785@qq.com>
2016-01-12 10:23:37 +0800

    修改多客服对语音转接客服的支持

--- Addons/YouaskService/Model/WeixinAddonModel.class.php
+++ Addons/YouaskService/Model/WeixinAddonModel.class.php
@@ -13,15 +13,15 @@ class WeixinAddonModel extends WeixinModel{
 		$config = getAddonConfig ( 'YouaskService' ); // 获取后台插件的配置参数	
 
 		$content = $dataArr["Content"];
-
+        			
 		//处理用户发送语音事件
 		if ($dataArr ['MsgType'] == 'voice') {
 			
-            $content = $dataArr['Recognition'];
-            $content = $this->filter_mark($content);
-        
+            $content = substr($dataArr['Recognition'],0,strlen($dataArr['Recognition'])-3); 
+
         }
-        
+
+
         //处理客服会话通知事件
         if ($dataArr ['MsgType'] == 'event') {
         	$event = strtolower ( $dataArr ['Event'] );
@@ -85,15 +85,6 @@ class WeixinAddonModel extends WeixinModel{
 		$this->zjtuling($dataArr, $keywordArr = array());
 
 	} 
-	//过滤所有标点符号
-    function filter_mark($text){ 
-       if(trim($text)=='')return ''; 
-          $text=preg_replace("/[[:punct:]\s]/",' ',$text); 
-          $text=urlencode($text); 
-          $text=preg_replace("/(%7E|%60|%21|%40|%23|%24|%25|%5E|%26|%27|%2A|%28|%29|%2B|%7C|%5C|%3D|\-|_|%5B|%5D|%7D|%7B|%3B|%22|%3A|%3F|%3E|%3C|%2C|\.|%2F|%A3%BF|%A1%B7|%A1%B6|%A1%A2|%A1%A3|%A3%AC|%7D|%A1%B0|%A3%BA|%A3%BB|%A1%AE|%A1%AF|%A1%B1|%A3%FC|%A3%BD|%A1%AA|%A3%A9|%A3%A8|%A1%AD|%A3%A4|%A1%A4|%A3%A1|%E3%80%82|%EF%BC%81|%EF%BC%8C|%EF%BC%9B|%EF%BC%9F|%EF%BC%9A|%E3%80%81|%E2%80%A6%E2%80%A6|%E2%80%9D|%E2%80%9C|%E2%80%98|%E2%80%99|%EF%BD%9E|%EF%BC%8E|%EF%BC%88)+/",' ',$text); 
-          $text=urldecode($text); 
-       return trim($text); 
-    } 
 
 	//转接人工客服
 	public function connectkf($content,$token,$config,$keywordArr,$dataArr){
@@ -417,9 +408,9 @@ class WeixinAddonModel extends WeixinModel{
 
 	}
 
-
-
-	
+	public function voice($data) {
+		$this->reply ($data, $keywordArr = array());
+	}
 
 
 	
