52eda5ad13fb454b50829105d2cdf120deb0e072
艾逗笔 <mikemintang@126.com>
2016-01-21 11:21:53 +0800

    修改自定义菜单插件数据安装卸载文件，增加个性化菜单支持

--- Addons/CustomMenu/uninstall.sql
+++ Addons/CustomMenu/uninstall.sql
@@ -2,4 +2,11 @@ DELETE FROM `wp_attribute` WHERE model_id = (SELECT id FROM wp_model WHERE `name
 DELETE FROM `wp_model` WHERE `name`='custom_menu' ORDER BY id DESC LIMIT 1;
 DROP TABLE IF EXISTS `wp_custom_menu`;
 
+DELETE FROM `wp_attribute` WHERE model_id = (SELECT id FROM wp_model WHERE `name`='custom_menu_type' ORDER BY id DESC LIMIT 1);
+DELETE FROM `wp_model` WHERE `name`='custom_menu_type' ORDER BY id DESC LIMIT 1;
+DROP TABLE IF EXISTS `wp_custom_menu_type`;
+
+
+
+
 
