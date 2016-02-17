8e197bea5cfb39ef97629bf53bad1cef254b1a36
艾逗笔 <mikemintang@126.com>
2016-02-17 09:39:53 +0800

    修改豆信显示版式，参考微擎布局

--- Addons/WeiSite/Controller/BaseController.class.php
+++ Addons/WeiSite/Controller/BaseController.class.php
@@ -15,20 +15,15 @@ class BaseController extends AddonsController {
 		$res ['url'] = addons_url ( 'WeiSite://WeiSite/config' );
 		$res ['class'] = $controller == 'weisite' ? 'current' : '';
 		$nav [] = $res;
-		 
-		$res ['title'] = '分类管理';
-		$res ['url'] = addons_url ( 'WeiSite://Category/lists' );
-		$res ['class'] = $controller == 'category' ? 'current' : '';
-		$nav [] = $res;
-		 
+
 		$res ['title'] = '首页幻灯片';
 		$res ['url'] = addons_url ( 'WeiSite://Slideshow/lists' );
 		$res ['class'] = $controller == 'slideshow' ? 'current' : '';
 		$nav [] = $res;
 		 
-		$res ['title'] = '底部导航';
-		$res ['url'] = addons_url ( 'WeiSite://Footer/lists' );
-		$res ['class'] = $controller == 'footer' ? 'current' : '';
+		$res ['title'] = '分类管理';
+		$res ['url'] = addons_url ( 'WeiSite://Category/lists' );
+		$res ['class'] = $controller == 'category' ? 'current' : '';
 		$nav [] = $res;
 		 
 		$res ['title'] = '文章管理';
@@ -36,6 +31,11 @@ class BaseController extends AddonsController {
 		$res ['class'] = $controller == 'cms' ? 'current' : '';
 		$nav [] = $res;
 		 
+		$res ['title'] = '底部导航';
+		$res ['url'] = addons_url ( 'WeiSite://Footer/lists' );
+		$res ['class'] = $controller == 'footer' ? 'current' : '';
+		$nav [] = $res;
+		
 		$res ['title'] = '模板管理';
 		$res ['url'] = addons_url ( 'WeiSite://Template/index' );
 		$res ['class'] = $controller == 'template' ? 'current' : '';
