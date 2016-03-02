460b61c8f7689471029160070df7388a1ccabfc8
艾逗笔 <mikemintang@126.com>
2016-03-02 14:32:42 +0800

    update usercenter title

--- Addons/UserCenter/UserCenterAddon.class.php
+++ Addons/UserCenter/UserCenterAddon.class.php
@@ -5,14 +5,14 @@ namespace Addons\UserCenter;
 use Common\Controller\Addon;
 
 /**
- * 用户管理插件
+ * 粉丝营销插件
  *
  * @author 艾逗笔
  */
 class UserCenterAddon extends Addon {
 	public $info = array (
 			'name' => 'UserCenter',
-			'title' => '用户管理',
+			'title' => '粉丝营销',
 			'description' => '实现3G首页、微信登录，微信用户绑定，微信用户信息初始化等基本功能',
 			'status' => 1,
 			'author' => '艾逗笔',
