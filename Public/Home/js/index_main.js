d49f950261747f4d3e94e47ba3e8905f0a0f12c8
艾逗笔 <mikemintang@126.com>
2016-03-07 11:30:24 +0800

    修改公众号管理首页

--- Public/Home/js/index_main.js
+++ Public/Home/js/index_main.js
@@ -5,7 +5,7 @@
           
           // 获取艾豆问答社区开发交流版块的数据
             $.ajax({
-                 url:remote_url+'wenda/?/interface/question/',
+                 url:remote_url+'?/interface/question/',
                  dataType: 'jsonp', 
                  data:'callback=jsonpcallback_communication',  
                  jsonp:'callback', 
@@ -13,7 +13,7 @@
 
             // 获取艾豆问答社区开发教程版块的数据
             $.ajax({
-                 url:remote_url+'wenda/?/interface/article/',
+                 url:remote_url+'?/interface/article/',
                  dataType: 'jsonp', 
                  data:'callback=jsonpcallback_course',  
                  jsonp:'callback',
@@ -21,21 +21,21 @@
 
             // 获取艾豆问答社区应用商城版块的数据
             $.ajax({
-                 url:remote_url+'wenda/?/interface/shop/',
+                 url:remote_url+'?/interface/shop/',
                  dataType: 'jsonp', 
                  data:'callback=jsonpcallback_appstore',  
                  jsonp:'callback',
             });
 
             $.ajax({
-                 url:remote_url+'wenda/?/interface/notice/',
+                 url:remote_url+'?/interface/notice/',
                  dataType: 'jsonp', 
                  data:'callback=jsonpcallback_notice',  
                  jsonp:'callback', 
             });
 
             $.ajax({
-                 url:remote_url+'wenda/?/interface/feedback/',
+                 url:remote_url+'?/interface/feedback/',
                  dataType: 'jsonp', 
                  data:'callback=jsonpcallback_feedback',  
                  jsonp:'callback', 
@@ -82,7 +82,7 @@
           $.each(data,function(i,item){
             if ( area == 'communication' ){
               var item = '<li class="mp_news_item">'+
-                      '<a href="'+remote_url+'wenda/?/question/'+item.question_id+'" target="_blank" title="'+data[i].title+'">'+
+                      '<a href="'+remote_url+'?/question/'+item.question_id+'" target="_blank" title="'+data[i].title+'">'+
                           '<strong style="width:300px;">'+data[i].question_content;
                       if (i<3){
                         item += '<i class="icon_common new"></i>';
@@ -94,7 +94,7 @@
                   '</li>';
               } else if ( area == 'course' ) {
                 var item = '<li class="mp_news_item">'+
-                    '<a href="'+remote_url+'wenda/?/article/'+item.id+'" target="_blank" title="'+data[i].title+'">'+
+                    '<a href="'+remote_url+'?/article/'+item.id+'" target="_blank" title="'+data[i].title+'">'+
                         '<strong style="width:300px;">'+data[i].title
                     if (i<3){
                       item += '<i class="icon_common new"></i>';
@@ -106,7 +106,7 @@
                   '</li>';
               } else if ( area == 'notice' ) {
                 var item = '<li class="mp_news_item">'+
-                    '<a href="'+remote_url+'wenda/?/notice/'+item.id+'" target="_blank" title="'+data[i].title+'">'+
+                    '<a href="'+remote_url+'?/notice/'+item.id+'" target="_blank" title="'+data[i].title+'">'+
                         '<strong style="width:300px;">'+data[i].title
                     if (i<3){
                       item += '<i class="icon_common new"></i>';
@@ -118,7 +118,7 @@
                   '</li>';
               } else if ( area == 'feedback' ) {
                 var item = '<li class="mp_news_item">'+
-                    '<a href="'+remote_url+'wenda/?/feedback/'+item.id+'" target="_blank" title="'+data[i].title+'">'+
+                    '<a href="'+remote_url+'?/feedback/'+item.id+'" target="_blank" title="'+data[i].title+'">'+
                         '<strong style="width:300px;">【'+data[i].category_id+'BUG】'+data[i].title
                     if (i<3){
                       item += '<i class="icon_common new"></i>';
@@ -148,8 +148,8 @@
                 var price = data[i].price+'豆币';
               }
               var item = '<li class="mp_news_item">'+
-                      '<a href="'+remote_url+'wenda/?/shop/'+item.id+'" target="_blank" title="'+data[i].title+'">'+
-                        '<img src="'+remote_url+'/wenda/'+data[i].cover+'">'+
+                      '<a href="'+remote_url+'?/shop/'+item.id+'" target="_blank" title="'+data[i].title+'">'+
+                        '<img src="'+remote_url+'/'+data[i].cover+'">'+
                         '<h4 class="title">'+data[i].title+'</h4>'+
                         '<h4 class="price">售价：<span style="color:#ff6600;">'+price+'</span><span class="view">'+data[i].views+'人看过</span></h4>'+
                       '</a>'+
